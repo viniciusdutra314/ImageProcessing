01110a2279f489bdd1867de98fabac2f01e3d947
viniciusdutra314 <dutravinisousa2@usp.br>
2026-04-13 18:00:06 -0300

    feat imagicMagick commands

--- ex03/app/Main.hs
+++ ex03/app/Main.hs
@@ -1,5 +1,53 @@
+import Control.Monad (forM_)
 import Matrix
 import PGM
+import System.Process (callCommand)
+
+imageMagick_ex02 :: String -> IO ()
+imageMagick_ex02 filepath = do
+  let command =
+        unwords
+          [ "magick montage",
+            "-label 'Padding : Zeros'",
+            "item02/checkboard_zeros_a.pgm",
+            "-label 'Padding : Espelhamento'",
+            "item02/checkboard_espelhamento_b.pgm",
+            "-label 'Padding : Replicação'",
+            "item02/checkboard_replicacao_c.pgm",
+            "-label 'Padding : Periódico'",
+            "item02/checkboard_periodico_d.pgm",
+            "-tile 2x2",
+            "-geometry +10+10",
+            "-background '#1982bf'",
+            "-fill '#333333'",
+            "-font 'DejaVu-Sans'",
+            "-pointsize 32",
+            filepath
+          ]
+  callCommand command
+
+imageMagick_ex03 :: String -> IO ()
+imageMagick_ex03 filepath = do
+  let command =
+        unwords
+          [ "magick montage",
+            "-label 'Imagem original'",
+            "img.pgm",
+            "-label 'Suavização Leve (5x5)'",
+            "item03/img_blur_5.pgm",
+            "-label 'Suavização Média (15x15)'",
+            "item03/img_blur_15.pgm",
+            "-label 'Suavização Forte (50x50)'",
+            "item03/img_blur_50.pgm",
+            "-tile 4x1",
+            "-geometry +10+10",
+            "-background '#1982bf'",
+            "-fill white",
+            "-font 'DejaVu-Sans'",
+            "-pointsize 50",
+            filepath
+          ]
+  callCommand command
 
 main :: IO ()
 main = do
@@ -18,10 +66,16 @@ main = do
           black = 0
           size = 512
           mid = size `div` 2
-  writeImagePGM "checkboard.pgm" checkboard
+  writeImagePGM "item01/checkboard.pgm" checkboard
   -- Exercício 2)
-  let kernel = createMatrixWithValue (15, 15) (1.0 / (15.0 * 15.0))
-  writeImagePGM "checkboard_zeros_a.pgm" (convolve checkboard kernel Zeroed)
-  writeImagePGM "checkboard_espelhamento_b.pgm" (convolve checkboard kernel Mirrored)
-  writeImagePGM "checkboard_replicacao_c.pgm" (convolve checkboard kernel Replication)
-  writeImagePGM "checkboard_periodico_d.pgm" (convolve checkboard kernel Periodic)
+  writeImagePGM "item02/checkboard_zeros_a.pgm" (convolve checkboard (kernelUniform 15) Zeroed)
+  writeImagePGM "item02/checkboard_espelhamento_b.pgm" (convolve checkboard (kernelUniform 15) Mirrored)
+  writeImagePGM "item02/checkboard_replicacao_c.pgm" (convolve checkboard (kernelUniform 15) Replication)
+  writeImagePGM "item02/checkboard_periodico_d.pgm" (convolve checkboard (kernelUniform 15) Periodic)
+  imageMagick_ex02 "item02/item02.ppm"
+  -- Exercício 3)
+  img <- readImagePGM "img.pgm" >>= either error pure
+  let ex03_kernel_sizes = [5, 15, 50]
+  forM_ ex03_kernel_sizes $ \n ->
+    writeImagePGM ("item03/img_blur_" ++ show n ++ ".pgm") (convolve img (kernelUniform n) Periodic)
+  imageMagick_ex03 "item03/item03.ppm"
--- ex03/app/Matrix.hs
+++ ex03/app/Matrix.hs
@@ -1,6 +1,9 @@
 module Matrix
   ( Matrix (..),
     ConvolveStrategy (..),
+    kernelUniform,
+    kernelGaussian,
+    matrixNormalize,
     convolve,
     createMatrixWithValue,
     createMatrixWithFunc,
@@ -37,19 +40,25 @@ createMatrixWithValue (rows, cols) val = Matrix rows cols (U.replicate (fromInte
 unflatIndex :: Int -> Int -> (Int, Int)
 unflatIndex cols i = (fromIntegral (i `div` fromIntegral cols), fromIntegral (i `mod` fromIntegral cols))
 
+createMatrix :: (U.Unbox a) => [[a]] -> Matrix a
+createMatrix nested_list = Matrix num_rows num_cols (U.fromList $ concat nested_list)
+  where
+    num_rows = length nested_list
+    num_cols = length $ head nested_list
+
 createMatrixWithFunc :: (U.Unbox a) => (Int, Int) -> ((Int, Int) -> a) -> Matrix a
 createMatrixWithFunc (rows, cols) func = Matrix rows cols (U.generate size (func . unflatenCurried))
   where
     size = fromIntegral (rows * cols)
     unflatenCurried = unflatIndex cols
 
-index :: (U.Unbox a) => Matrix a -> (Int, Int) -> a
-index (Matrix _ m elements) (r, c) = elements U.! fromIntegral (r * m + c)
+unsafeIndex :: (U.Unbox a) => Matrix a -> (Int, Int) -> a
+unsafeIndex (Matrix _ m elements) (r, c) = elements U.! fromIntegral (r * m + c)
 
-indexMaybe :: (U.Unbox a) => Matrix a -> (Int, Int) -> Maybe a
-indexMaybe matrix@(Matrix rows cols _) (r, c)
+index :: (U.Unbox a) => Matrix a -> (Int, Int) -> Maybe a
+index matrix@(Matrix rows cols _) (r, c)
   | r < 0 || r >= rows || c < 0 || c >= cols = Nothing
-  | otherwise = Just (index matrix (r, c))
+  | otherwise = Just (unsafeIndex matrix (r, c))
 
 clamp :: (Ord a) => a -> a -> a -> a
 clamp min_val max_val x
@@ -73,7 +82,7 @@ convolve
             ( round
                 ( sum
                     [ sample (r - center + kr, c - center + kc)
-                        * (kernel `index` (kr, kc))
+                        * (kernel `unsafeIndex` (kr, kc))
                       | kr <- [0 .. rows_k - 1],
                         kc <- [0 .. cols_k - 1]
                     ]
@@ -85,10 +94,10 @@ convolve
       sample :: (Int, Int) -> Float
       sample (r, c) =
         case convolve_strategy of
-          Zeroed -> fromIntegral $ fromMaybe 0 (indexMaybe img (r, c)) :: Float
-          Mirrored -> fromIntegral $ index img (mirror1D rows r, mirror1D cols c) :: Float
-          Periodic -> fromIntegral $ index img (r `wrap` rows, c `wrap` cols) :: Float
-          Replication -> fromIntegral $ index img (clamp 0 (rows - 1) r, clamp 0 (cols - 1) c) :: Float
+          Zeroed -> fromIntegral $ fromMaybe 0 (index img (r, c)) :: Float
+          Mirrored -> fromIntegral $ unsafeIndex img (mirror1D rows r, mirror1D cols c) :: Float
+          Periodic -> fromIntegral $ unsafeIndex img (r `wrap` rows, c `wrap` cols) :: Float
+          Replication -> fromIntegral $ unsafeIndex img (clamp 0 (rows - 1) r, clamp 0 (cols - 1) c) :: Float
         where
           mirror1D :: Int -> Int -> Int
           mirror1D max_coord coord
@@ -99,3 +108,33 @@ convolve
           wrap x n
             | x > 0 = x `mod` n
             | otherwise = n - 1 - ((-x) `mod` n)
+
+kernelUniform :: Int -> Matrix Float
+kernelUniform n =
+  createMatrixWithValue (n, n) val
+  where
+    val = 1.0 / fromIntegral (n * n)
+
+matrixNormalize :: Matrix Float -> Matrix Float
+matrixNormalize m@(Matrix rows cols elements) =
+  createMatrixWithFunc
+    (rows, cols)
+    ( \(i, j) ->
+        (m `unsafeIndex` (i, j)) / total_sum
+    )
+  where
+    total_sum = U.sum elements
+
+kernelGaussian :: Int -> Float -> Matrix Float
+kernelGaussian n sigma =
+  matrixNormalize $
+    createMatrixWithFunc
+      (n, n)
+      ( \(i, j) ->
+          let ci = fromIntegral (i - center)
+              cj = fromIntegral (j - center)
+              r2 = ci * ci + cj * cj
+           in exp (-r2 / (2 * sigma * sigma))
+      )
+  where
+    center = n `div` 2
