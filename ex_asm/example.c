d1aa61718a16b668d59ce439a89d453cdf32b6e4
viniciusdutra314 <dutravinisousa2@usp.br>
2026-07-18 16:31:20 -0300

    feat: prime test

--- ex_asm/example.c
+++ ex_asm/example.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
-
-extern float kinectic_energy(float m, float v);
+#include <assert.h>
+extern int kinectic_energy(int m, int v);
+extern int is_prime(int x);
+extern int factorial(int x);
 
 int main() {
-  float m = 4.0;
-  float v = 7.0;
-  printf("m=%f\n", m);
-  printf("v=%f\n", v);
-  printf("kinetic_energy(m,v)=%f\n", kinectic_energy(m, v));
-  printf("expected %f\n", (m * v * v) / 2);
+  assert(!is_prime(0));
+  assert(!is_prime(1));
+  assert(is_prime(2));
+  assert(is_prime(3));
+  assert(!is_prime(4));
+  assert(is_prime(5));
+  assert(!is_prime(6));
+  assert(is_prime(7));
 }
--- ex_asm/example.s
+++ ex_asm/example.s
@@ -1,8 +1,49 @@
-.global kinectic_energy
+.global kinectic_energy,factorial,is_prime
+.type factorial,%function
 .type kinectic_energy,%function
+.type is_prime,%function
 .p2align 4
 
-kinectic_energy: //s0=m,s1=v
-    fmul s2, s1,s1 
-    fmul s0, s0,s2
+kinectic_energy: //x0=m,x1=v
+    mul x2, x1,x1 
+    mul x0, x0,x2
+    mov x3,#2
+    udiv x0, x0, x3
+    ret
+
+
+factorial: 
+    cmp x0,#1
+    beq done_factorial
+    cmp x0,#0 
+    beq done_factorial
+
+    mov x1,x0 
+loop_factorial: cmp x1,#1
+    beq done_factorial
+    sub x1,x1,#1
+    mul x0,x0,x1
+    b loop_factorial
+done_factorial:
+    ret
+
+
+is_prime:
+    cmp x0,#2
+    b.lo notprime
+    b.eq prime
+    mov x1,#2
+divide_loop:
+    udiv x2,x0,x1 
+    msub x2, x1,x2 ,x0 // p/q <=> p-q*udiv=remainder
+    cbz x2,notprime
+    add x1,x1,#1
+    cmp x1,x0
+    b.ne divide_loop
+    b.eq prime
+prime:
+    mov x0,#1
+    ret 
+notprime:
+    mov x0,#0
     ret
